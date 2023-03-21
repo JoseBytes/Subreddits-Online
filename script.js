6cd238e0966e656b83d3f0370f66dd576684a847
Kn4 <31645443+Kn4byte@users.noreply.github.com>
2023-03-20 22:56:47 -0400

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,91 +1,27 @@
 const subreddits = ['FootFetishExperiences', 'FeetLoversHeaven', 'Feet_NSFW', 'feetpics', 'VerifiedFeet', 'FootFetish', 'Feetishh', 'FeetInYourFace', 'sendfeetpics', 'VIPFeet', 'Rate_my_feet'];
 
-const apiUrl = 'https://www.reddit.com/';
+const subredditList = document.getElementById('subreddit-list');
 
-function getSubredditData(subreddit) {
-
-  return fetch(`${apiUrl}r/${subreddit}/about.json`)
-
+const promises = subreddits.map(subreddit => {
+  return fetch(`https://www.reddit.com/r/${subreddit}/about.json`)
     .then(response => response.json())
-
-    .then(data => ({
-
-      name: subreddit,
-
-      usersOnline: data.data.active_user_count
-
-    }))
-
-    .catch(error => console.error(error));
-
-}
-
-function updateSubredditData(subreddit) {
-
-  const subredditElement = document.querySelector(`#${subreddit}`);
-
-  getSubredditData(subreddit)
-
     .then(data => {
-
-      subredditElement.querySelector('.subreddit-users').textContent = `${data.usersOnline} users online`;
-
-      if (data.usersOnline > 400) {
-
-        subredditElement.querySelector('.subreddit-users').classList.add('green');
-
-        subredditElement.querySelector('.subreddit-users').classList.remove('red');
-
-      } else {
-
-        subredditElement.querySelector('.subreddit-users').classList.add('red');
-
-        subredditElement.querySelector('.subreddit-users').classList.remove('green');
-
-      }
-
+      return {
+        subreddit: subreddit,
+        onlineUsers: data.data.active_user_count
+      };
     })
-
     .catch(error => console.error(error));
+});
 
-}
-
-function renderSubreddits() {
-
-  const subredditsElement = document.querySelector('.subreddits');
-
-  subreddits.forEach(subreddit => {
-
-    const subredditElement = document.createElement('div');
-
-    subredditElement.classList.add('subreddit');
-
-    subredditElement.id = subreddit;
-
-    const subredditNameElement = document.createElement('div');
-
-    subredditNameElement.classList.add('subreddit-name');
-
-    subredditNameElement.textContent = subreddit;
-
-    subredditElement.appendChild(subredditNameElement);
-
-    const subredditUsersElement = document.createElement('div');
-
-    subredditUsersElement.classList.add('subreddit-users');
-
-    subredditElement.appendChild(subredditUsersElement);
-
-    subredditsElement.appendChild(subredditElement);
-
-    updateSubredditData(subreddit);
-
-    setInterval(() => updateSubredditData(subreddit), 10 * 1000);
-
+Promise.all(promises).then(results => {
+  results.sort((a, b) => b.onlineUsers - a.onlineUsers);
+  results.forEach(result => {
+    const listItem = document.createElement('li');
+    listItem.textContent = `${result.subreddit}: ${result.onlineUsers} usuarios en línea`;
+    if (result.onlineUsers > 400) {
+      listItem.style.color = 'green';
+    }
+    subredditList.appendChild(listItem);
   });
-
-}
-
-renderSubreddits();
-
-
+});
